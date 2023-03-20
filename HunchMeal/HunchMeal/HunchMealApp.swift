3cc2f507cb3d9dd47ee51943c6365bf9c6579411
Leonardo Wijaya <leonardo.wijaya003@binus.ac.id>
2023-03-20 17:48:52 +0700

    First Initialize

---
+++ HunchMeal/HunchMeal/HunchMealApp.swift
@@ -0,0 +1,17 @@
+//
+//  HunchMealApp.swift
+//  HunchMeal
+//
+//  Created by Leonardo Wijaya on 20/03/23.
+//
+
+import SwiftUI
+
+@main
+struct HunchMealApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
