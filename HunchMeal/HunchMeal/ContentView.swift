89f0711e42a1a62bc63889f2aef0e683d70c1dc8
jihanalfi <jihan.alfiyyah@ui.ac.id>
2023-03-22 21:21:32 +0700

    Landing Page 1

--- HunchMeal/HunchMeal/ContentView.swift
+++ HunchMeal/HunchMeal/ContentView.swift
@@ -9,13 +9,44 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+        ZStack() {
+            Circle()
+                .foregroundColor(Color("Yellow"))
+                .frame(width: 753, height: 753)
+                .fixedSize()
+                .offset(y: -160)
+            
+            VStack{
+                Text("Hunch")
+                    .font(.system(size: 88, weight: .bold))
+                    .foregroundColor(Color("Purple"))
+                    .navigationBarTitleDisplayMode(.inline)
+                    .offset(y: -98)
+                Text("Meal")
+                    .font(.system(size: 88, weight: .bold))
+                    .foregroundColor(Color("Purple"))
+                    .navigationBarTitleDisplayMode(.inline)
+                    .offset(y: -98)
+                
+            
+                
+                Button(action: {
+                    print("tapped")
+                }){
+                    Text("PLAY")
+                        .font(.system(size: 36, weight: .bold))
+                        .frame(width: 146, height: 146)
+                        .foregroundColor(Color("Yellow"))
+                        .background(Color("Purple"))
+                        .clipShape(Circle())
+                        .offset(y: -21)
+                }
+
+                
+            }
         }
         .padding()
+        .background(Color("Purple"))
     }
 }
 
