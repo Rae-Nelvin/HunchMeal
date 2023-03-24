1999de50adcfcf520c683f8f5aacfd6e9ac20434
jihanalfi <jihan.alfiyyah@ui.ac.id>
2023-03-24 15:00:40 +0700

    fix NavigationBar

--- HunchMeal/HunchMeal/Views/GameView.swift
+++ HunchMeal/HunchMeal/Views/GameView.swift
@@ -10,6 +10,7 @@ import SwiftUI
 struct GameView: View {
     @State private var showLandingPage = false
 
+
     var body: some View {
         ZStack(){
             BackgroundYellowCircle()
@@ -51,40 +52,8 @@ struct GameView: View {
                     }
                     .padding(.bottom, 125)
                     
-                    
-                    NavigationLink("", destination: HunchMealView())
-                        .toolbar{
-                            ToolbarItem(placement: .navigationBarLeading){
-                                Button (action: {
-                                    showLandingPage = true
-                                }){
-                                    Image(systemName: "arrow.left.square.fill")
-                                        .font(.system(size: 40.0, weight: .medium))
-                                        .imageScale(.large)
-                                        .foregroundColor(Color("Purple"))
-                                }
-                            }
-                            
-                            ToolbarItem(placement: .navigationBarTrailing){
-                                Image(systemName: "questionmark.square.fill")
-                                    .font(.system(size: 40.0, weight: .medium))
-                                    .imageScale(.large)
-                                    .foregroundColor(Color("Purple"))
-                            }
-                            
-                            ToolbarItem(placement: .principal){
-                                Text("00:00")
-                                    .frame(width: 80.0, height: 25.0)
-                                    .padding(9)
-                                    .font(.system(size: 24, design: .rounded).weight(.bold))
-                                    .foregroundColor(Color("Purple"))
-                                    .overlay(
-                                        RoundedRectangle(cornerRadius: 7)
-                                            .stroke(Color("Purple"), lineWidth: 4))
-                                    .imageScale(.large)
-                            }
-                        }
-                        .navigationBarBackButtonHidden(true)
+                    NavigationBar(showLandingPage: $showLandingPage)
+            
                 }
             }
         }
@@ -119,3 +88,44 @@ struct SmallCardView: View {
         }
     }
 }
+
+struct NavigationBar: View{
+    @Binding var showLandingPage:Bool
+
+    var body: some View {
+        NavigationLink("", destination: HunchMealView())
+            .toolbar{
+                ToolbarItem(placement: .navigationBarLeading){
+                    Button (action: {
+                        showLandingPage = true
+                    }){
+                        Image(systemName: "arrow.left.square.fill")
+                            .font(.system(size: 40.0, weight: .medium))
+                            .imageScale(.large)
+                            .foregroundColor(Color("Purple"))
+                    }
+                }
+                
+                ToolbarItem(placement: .navigationBarTrailing){
+                    Image(systemName: "questionmark.square.fill")
+                        .font(.system(size: 40.0, weight: .medium))
+                        .imageScale(.large)
+                        .foregroundColor(Color("Purple"))
+                }
+                
+                ToolbarItem(placement: .principal){
+                    Text("00:00")
+                        .frame(width: 80.0, height: 25.0)
+                        .padding(9)
+                        .font(.system(size: 24, design: .rounded).weight(.bold))
+                        .foregroundColor(Color("Purple"))
+                        .overlay(
+                            RoundedRectangle(cornerRadius: 7)
+                                .stroke(Color("Purple"), lineWidth: 4))
+                        .imageScale(.large)
+                }
+            }
+            .navigationBarBackButtonHidden(true)
+    }
+}
+
